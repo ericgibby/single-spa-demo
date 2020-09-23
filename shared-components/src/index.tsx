48bc9b5300da392f50bd0cfb2da2334cfa0444f6
Eric Gibby <eric.gibby@imaginelearning.com>
2020-09-23 09:29:15 -0600

    Created shared component library

---
+++ shared-components/src/index.tsx
@@ -0,0 +1,2 @@
+export * from './error-fallback/error-fallback.component';
+export * from './single-spa-link/single-spa-link.component';
---
+++ shared-components/src/single-spa-link/single-spa-link.component.tsx
@@ -0,0 +1,20 @@
+import React, { AnchorHTMLAttributes, ReactNode, SyntheticEvent } from 'react';
+import { navigateToUrl } from 'single-spa';
+
+type SingleSpaLinkProps = {
+	children?: ReactNode;
+	to: string;
+};
+
+export function SingleSpaLink({ children, to, ...props }: SingleSpaLinkProps & AnchorHTMLAttributes<ReactNode>) {
+	const handleClick = (e: SyntheticEvent) => {
+		e.preventDefault();
+		navigateToUrl(to);
+	};
+
+	return (
+		<a {...props} href={to} onClick={handleClick}>
+			{children}
+		</a>
+	);
+}
