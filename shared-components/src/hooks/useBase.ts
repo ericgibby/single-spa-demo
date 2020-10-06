5f5f44f40174c8d13e071e8cc79e5ea5c83726b8
Eric Gibby <eric.gibby@imaginelearning.com>
2020-10-06 15:20:20 -0600

    Removed unnecessary dependency

--- shared-components/src/hooks/useBase.ts
+++ shared-components/src/hooks/useBase.ts
@@ -4,6 +4,6 @@ export function useBase() {
 	const base = useMemo(() => {
 		const { pathname } = new URL(document.baseURI);
 		return pathname;
-	}, [document.baseURI]);
+	}, []);
 	return base;
 }
