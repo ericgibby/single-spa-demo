6e7b89ded7b6f9e24923dc401852ccb0e8c72cd6
Eric Gibby <eric.gibby@imaginelearning.com>
2020-10-06 20:28:38 -0600

    Hooked up auth-app with Redux

--- auth-app/src/root.component.tsx
+++ auth-app/src/root.component.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { Action, Store } from 'redux';
 import AuthProvider from './components/AuthProvider/AuthProvider';
 import Callback from './components/Callback/Callback';
 import Signin from './components/Signin/Signin';
@@ -9,19 +11,26 @@ const clientId = process.env.OAUTH_CLIENT_ID;
 
 type RootProps = {
 	name: string;
+	store?: Store;
+	tokenReceivedAction?: (token: string) => Action;
 };
 
-export default function Root(props: RootProps) {
+export default function Root({ store, tokenReceivedAction }: RootProps) {
+	const handleTokenReceived = (token: string) => store?.dispatch(tokenReceivedAction?.(token));
 	return (
 		<ErrorBoundary fallback={<h1>Error!</h1>}>
 			<AuthProvider clientId={clientId}>
-				<BrowserRouter basename="/single-spa-demo">
-					<Switch>
-						<Route path="/auth/callback" component={Callback} />
-						<Route path="/auth/signin" component={Signin} />
-						<Redirect path="/auth" to="/auth/signin" />
-					</Switch>
-				</BrowserRouter>
+				<Provider store={store}>
+					<BrowserRouter basename="/single-spa-demo">
+						<Switch>
+							<Route path="/auth/callback">
+								<Callback onTokenReceived={handleTokenReceived} />
+							</Route>
+							<Route path="/auth/signin" component={Signin} />
+							<Redirect path="/auth" to="/auth/signin" />
+						</Switch>
+					</BrowserRouter>
+				</Provider>
 			</AuthProvider>
 		</ErrorBoundary>
 	);
